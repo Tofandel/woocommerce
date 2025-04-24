Tofandel/woocommerce

commit bbd5b7ab8ebf42b32a7c048c92f7d9b0553f4df3

Add brands support to Store API (#55305)
* Add brands support to Store API

* Filter products by brand and return brands on product responses

* continue using link

* Add changefile(s) from automation for the following project(s): woocommerce

* fix linter

* add tests

* fix markdown issues

---------

Co-authored-by: github-actions <github-actions@github.com>

plugins/woocommerce/src/StoreApi/Utilities/ProductQuery.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 namespace Automattic\WooCommerce\StoreApi\Utilities;
 
 use Automattic\WooCommerce\Enums\ProductStatus;
@@ -111,8 +113,9 @@ class ProductQuery {
 
 		// Map between taxonomy name and arg key.
 		$default_taxonomies = array(
-			'product_cat' => 'category',
-			'product_tag' => 'tag',
+			'product_cat'   => 'category',
+			'product_tag'   => 'tag',
+			'product_brand' => 'brand',
 		);
 
 		$taxonomies = array_merge( $all_product_taxonomies, $default_taxonomies );
@@ -120,10 +123,11 @@ class ProductQuery {
 		// Set tax_query for each passed arg.
 		foreach ( $taxonomies as $taxonomy => $key ) {
 			if ( ! empty( $request[ $key ] ) ) {
+				$type        = is_numeric( $request[ $key ][0] ) ? 'term_id' : 'slug';
 				$operator    = $request->get_param( $key . '_operator' ) && isset( $operator_mapping[ $request->get_param( $key . '_operator' ) ] ) ? $operator_mapping[ $request->get_param( $key . '_operator' ) ] : 'IN';
 				$tax_query[] = array(
 					'taxonomy' => $taxonomy,
-					'field'    => 'term_id',
+					'field'    => $type,
 					'terms'    => $request[ $key ],
 					'operator' => $operator,
 				);

plugins/woocommerce/src/StoreApi/Routes/V1/ProductBrands.php
@@ -1,0 +1,69 @@
+<?php
+declare(strict_types=1);
+
+namespace Automattic\WooCommerce\StoreApi\Routes\V1;
+
+/**
+ * ProductBrands class.
+ */
+class ProductBrands extends AbstractTermsRoute {
+	/**
+	 * The route identifier.
+	 *
+	 * @var string
+	 */
+	const IDENTIFIER = 'product-brands';
+
+	/**
+	 * The routes schema.
+	 *
+	 * @var string
+	 */
+	const SCHEMA_TYPE = 'product-brand';
+
+	/**
+	 * Get the path of this REST route.
+	 *
+	 * @return string
+	 */
+	public function get_path() {
+		return self::get_path_regex();
+	}
+
+	/**
+	 * Get the path of this rest route.
+	 *
+	 * @return string
+	 */
+	public static function get_path_regex() {
+		return '/products/brands';
+	}
+
+	/**
+	 * Get method arguments for this REST route.
+	 *
+	 * @return array An array of endpoints.
+	 */
+	public function get_args() {
+		return [
+			[
+				'methods'             => \WP_REST_Server::READABLE,
+				'callback'            => [ $this, 'get_response' ],
+				'permission_callback' => '__return_true',
+				'args'                => $this->get_collection_params(),
+				'allow_batch'         => [ 'v1' => true ],
+			],
+			'schema' => [ $this->schema, 'get_public_item_schema' ],
+		];
+	}
+
+	/**
+	 * Get a collection of terms.
+	 *
+	 * @param \WP_REST_Request $request Request object.
+	 * @return \WP_REST_Response
+	 */
+	protected function get_route_response( \WP_REST_Request $request ) {
+		return $this->get_terms_response( 'product_brand', $request );
+	}
+}

plugins/woocommerce/src/StoreApi/Routes/V1/ProductBrandsById.php
@@ -1,0 +1,99 @@
+<?php
+declare(strict_types=1);
+
+namespace Automattic\WooCommerce\StoreApi\Routes\V1;
+
+use Automattic\WooCommerce\StoreApi\Exceptions\RouteException;
+
+/**
+ * ProductBrandsById class.
+ */
+class ProductBrandsById extends AbstractRoute {
+	/**
+	 * The route identifier.
+	 *
+	 * @var string
+	 */
+	const IDENTIFIER = 'product-brands-by-id';
+
+	/**
+	 * The routes schema.
+	 *
+	 * @var string
+	 */
+	const SCHEMA_TYPE = 'product-brand';
+
+	/**
+	 * Get the path of this REST route.
+	 *
+	 * @return string
+	 */
+	public function get_path() {
+		return self::get_path_regex();
+	}
+
+	/**
+	 * Get the path of this rest route.
+	 *
+	 * @return string
+	 */
+	public static function get_path_regex() {
+		return '/products/brands/(?P<identifier>[\w-]+)';
+	}
+
+	/**
+	 * Get method arguments for this REST route.
+	 *
+	 * @return array An array of endpoints.
+	 */
+	public function get_args() {
+		return [
+			'args'   => array(
+				'identifier' => array(
+					'description' => __( 'Unique identifier for the resource.', 'woocommerce' ),
+					'type'        => 'string',
+				),
+			),
+			[
+				'methods'             => \WP_REST_Server::READABLE,
+				'callback'            => [ $this, 'get_response' ],
+				'permission_callback' => '__return_true',
+				'args'                => array(
+					'context' => $this->get_context_param(
+						array(
+							'default' => 'view',
+						)
+					),
+				),
+				'allow_batch'         => [ 'v1' => true ],
+			],
+			'schema' => [ $this->schema, 'get_public_item_schema' ],
+		];
+	}
+
+	/**
+	 * Get a single item.
+	 *
+	 * @throws RouteException On error.
+	 * @param \WP_REST_Request $request Request object.
+	 * @return \WP_REST_Response
+	 */
+	protected function get_route_response( \WP_REST_Request $request ) {
+		if ( isset( $request['identifier'] ) && is_numeric( $request['identifier'] ) ) {
+			$object = get_term( (int) $request['identifier'], 'product_brand' );
+		} else {
+			$object = get_term_by( 'slug', $request['identifier'], 'product_brand' );
+		}
+
+		if ( ! $object ) {
+			if ( isset( $request['identifier'] ) && is_numeric( $request['identifier'] ) ) {
+				throw new RouteException( 'woocommerce_rest_brand_invalid_id', esc_html__( 'Invalid brand ID.', 'woocommerce' ), 404 );
+			} else {
+				throw new RouteException( 'woocommerce_rest_brand_invalid_slug', esc_html__( 'Invalid brand slug.', 'woocommerce' ), 404 );
+			}
+		}
+
+		$data = $this->prepare_item_for_response( $object, $request );
+		return rest_ensure_response( $data );
+	}
+}

plugins/woocommerce/src/StoreApi/Routes/V1/Products.php
@@ -287,14 +287,30 @@ class Products extends AbstractRoute {
 		);
 
 		$params['category'] = array(
-			'description'       => __( 'Limit result set to products assigned a specific category ID.', 'woocommerce' ),
+			'description'       => __( 'Limit result set to products assigned a set of category IDs or slugs, separated by commas.', 'woocommerce' ),
 			'type'              => 'string',
-			'sanitize_callback' => 'wp_parse_id_list',
+			'sanitize_callback' => 'wp_parse_list',
 			'validate_callback' => 'rest_validate_request_arg',
 		);
 
 		$params['category_operator'] = array(
 			'description'       => __( 'Operator to compare product category terms.', 'woocommerce' ),
+			'type'              => 'string',
+			'enum'              => [ 'in', 'not_in', 'and' ],
+			'default'           => 'in',
+			'sanitize_callback' => 'sanitize_key',
+			'validate_callback' => 'rest_validate_request_arg',
+		);
+
+		$params['brand'] = array(
+			'description'       => __( 'Limit result set to products assigned a set of brand IDs or slugs, separated by commas.', 'woocommerce' ),
+			'type'              => 'string',
+			'sanitize_callback' => 'wp_parse_list',
+			'validate_callback' => 'rest_validate_request_arg',
+		);
+
+		$params['brand_operator'] = array(
+			'description'       => __( 'Operator to compare product brand terms.', 'woocommerce' ),
 			'type'              => 'string',
 			'enum'              => [ 'in', 'not_in', 'and' ],
 			'default'           => 'in',
@@ -310,15 +326,15 @@ class Products extends AbstractRoute {
 
 			if ( str_starts_with( $param, '_unstable_tax_' ) && ! str_ends_with( $param, '_operator' ) ) {
 				$params[ $param ] = array(
-					'description'       => __( 'Limit result set to products assigned a specific category ID.', 'woocommerce' ),
+					'description'       => __( 'Limit result set to products assigned a set of taxonomies IDs or slugs, separated by commas.', 'woocommerce' ),
 					'type'              => 'string',
-					'sanitize_callback' => 'wp_parse_id_list',
+					'sanitize_callback' => 'wp_parse_list',
 					'validate_callback' => 'rest_validate_request_arg',
 				);
 			}
 			if ( str_starts_with( $param, '_unstable_tax_' ) && str_ends_with( $param, '_operator' ) ) {
 				$params[ $param ] = array(
-					'description'       => __( 'Operator to compare product category terms.', 'woocommerce' ),
+					'description'       => __( 'Operator to compare product taxonomies terms.', 'woocommerce' ),
 					'type'              => 'string',
 					'enum'              => [ 'in', 'not_in', 'and' ],
 					'default'           => 'in',
@@ -329,9 +345,9 @@ class Products extends AbstractRoute {
 		}
 
 		$params['tag'] = array(
-			'description'       => __( 'Limit result set to products assigned a specific tag ID.', 'woocommerce' ),
+			'description'       => __( 'Limit result set to products assigned a set of tag IDs or slugs, separated by commas.', 'woocommerce' ),
 			'type'              => 'string',
-			'sanitize_callback' => 'wp_parse_id_list',
+			'sanitize_callback' => 'wp_parse_list',
 			'validate_callback' => 'rest_validate_request_arg',
 		);
 

plugins/woocommerce/src/StoreApi/RoutesController.php
@@ -61,6 +61,8 @@ class RoutesController {
 				Routes\V1\ProductAttributeTerms::IDENTIFIER => Routes\V1\ProductAttributeTerms::class,
 				Routes\V1\ProductCategories::IDENTIFIER  => Routes\V1\ProductCategories::class,
 				Routes\V1\ProductCategoriesById::IDENTIFIER => Routes\V1\ProductCategoriesById::class,
+				Routes\V1\ProductBrands::IDENTIFIER      => Routes\V1\ProductBrands::class,
+				Routes\V1\ProductBrandsById::IDENTIFIER  => Routes\V1\ProductBrandsById::class,
 				Routes\V1\ProductCollectionData::IDENTIFIER => Routes\V1\ProductCollectionData::class,
 				Routes\V1\ProductReviews::IDENTIFIER     => Routes\V1\ProductReviews::class,
 				Routes\V1\ProductTags::IDENTIFIER        => Routes\V1\ProductTags::class,

plugins/woocommerce/src/StoreApi/SchemaController.php
@@ -52,6 +52,7 @@ class SchemaController {
 				Schemas\V1\ProductSchema::IDENTIFIER       => Schemas\V1\ProductSchema::class,
 				Schemas\V1\ProductAttributeSchema::IDENTIFIER => Schemas\V1\ProductAttributeSchema::class,
 				Schemas\V1\ProductCategorySchema::IDENTIFIER => Schemas\V1\ProductCategorySchema::class,
+				Schemas\V1\ProductBrandSchema::IDENTIFIER  => Schemas\V1\ProductBrandSchema::class,
 				Schemas\V1\ProductCollectionDataSchema::IDENTIFIER => Schemas\V1\ProductCollectionDataSchema::class,
 				Schemas\V1\ProductReviewSchema::IDENTIFIER => Schemas\V1\ProductReviewSchema::class,
 				Schemas\V1\AI\ProductsSchema::IDENTIFIER   => Schemas\V1\AI\ProductsSchema::class,

plugins/woocommerce/src/StoreApi/Schemas/V1/ProductBrandSchema.php
@@ -1,0 +1,126 @@
+<?php
+declare(strict_types=1);
+
+namespace Automattic\WooCommerce\StoreApi\Schemas\V1;
+
+use Automattic\WooCommerce\StoreApi\SchemaController;
+use Automattic\WooCommerce\StoreApi\Schemas\ExtendSchema;
+
+
+/**
+ * ProductBrandSchema class.
+ */
+class ProductBrandSchema extends TermSchema {
+	/**
+	 * The schema item name.
+	 *
+	 * @var string
+	 */
+	protected $title = 'product-brand';
+
+	/**
+	 * The schema item identifier.
+	 *
+	 * @var string
+	 */
+	const IDENTIFIER = 'product-brand';
+
+	/**
+	 * Image attachment schema instance.
+	 *
+	 * @var ImageAttachmentSchema
+	 */
+	protected $image_attachment_schema;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param ExtendSchema     $extend Rest Extending instance.
+	 * @param SchemaController $controller Schema Controller instance.
+	 */
+	public function __construct( ExtendSchema $extend, SchemaController $controller ) {
+		parent::__construct( $extend, $controller );
+		$this->image_attachment_schema = $this->controller->get( ImageAttachmentSchema::IDENTIFIER );
+	}
+
+	/**
+	 * Term properties.
+	 *
+	 * @return array
+	 */
+	public function get_properties() {
+		$schema                 = parent::get_properties();
+		$schema['image']        = [
+			'description' => __( 'Brand image.', 'woocommerce' ),
+			'type'        => 'object',
+			'context'     => [ 'view', 'edit', 'embed' ],
+			'readonly'    => true,
+			'properties'  => $this->image_attachment_schema->get_properties(),
+		];
+		$schema['review_count'] = [
+			'description' => __( 'Number of reviews for products of this brand.', 'woocommerce' ),
+			'type'        => 'integer',
+			'context'     => [ 'view', 'edit' ],
+			'readonly'    => true,
+		];
+		$schema['permalink']    = [
+			'description' => __( 'Brand URL.', 'woocommerce' ),
+			'type'        => 'string',
+			'format'      => 'uri',
+			'context'     => [ 'view', 'edit', 'embed' ],
+			'readonly'    => true,
+		];
+		return $schema;
+	}
+
+	/**
+	 * Convert a term object into an object suitable for the response.
+	 *
+	 * @param \WP_Term $term Term object.
+	 * @return array
+	 */
+	public function get_item_response( $term ) {
+		$response = parent::get_item_response( $term );
+		$count    = get_term_meta( $term->term_id, 'product_count_product_brand', true );
+
+		if ( $count ) {
+			$response['count'] = (int) $count;
+		}
+
+		$response['image']        = $this->image_attachment_schema->get_item_response( get_term_meta( $term->term_id, 'thumbnail_id', true ) );
+		$response['review_count'] = $this->get_brand_review_count( $term );
+		$response['permalink']    = get_term_link( $term->term_id, 'product_brand' );
+
+		return $response;
+	}
+
+	/**
+	 * Get total number of reviews for products of a brand.
+	 *
+	 * @param \WP_Term $term Term object.
+	 * @return int
+	 */
+	protected function get_brand_review_count( $term ) {
+		global $wpdb;
+
+		$children = get_term_children( $term->term_id, 'product_brand' );
+
+		if ( ! $children || is_wp_error( $children ) ) {
+			$terms_to_count_str = absint( $term->term_id );
+		} else {
+			$terms_to_count     = array_unique( array_map( 'absint', array_merge( array( $term->term_id ), $children ) ) );
+			$terms_to_count_str = implode( ',', $terms_to_count );
+		}
+
+		$products_of_brand_sql = "
+			SELECT SUM(comment_count) as review_count
+			FROM {$wpdb->posts} AS posts
+			INNER JOIN {$wpdb->term_relationships} AS term_relationships ON posts.ID = term_relationships.object_id
+			WHERE term_relationships.term_taxonomy_id IN (" . esc_sql( $terms_to_count_str ) . ')
+		';
+
+		$review_count = $wpdb->get_var( $products_of_brand_sql ); // phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared
+
+		return (int) $review_count;
+	}
+}

plugins/woocommerce/src/StoreApi/Schemas/V1/ProductSchema.php
@@ -248,7 +248,41 @@ class ProductSchema extends AbstractSchema {
 							'readonly'    => true,
 						],
 						'link' => [
-							'description' => __( 'Tag link', 'woocommerce' ),
+							'description' => __( 'Tag link.', 'woocommerce' ),
+							'type'        => 'string',
+							'context'     => [ 'view', 'edit' ],
+							'readonly'    => true,
+						],
+					],
+				],
+			],
+			'brands'              => [
+				'description' => __( 'List of brands, if applicable.', 'woocommerce' ),
+				'type'        => 'array',
+				'context'     => [ 'view', 'edit' ],
+				'items'       => [
+					'type'       => 'object',
+					'properties' => [
+						'id'   => [
+							'description' => __( 'Brand ID', 'woocommerce' ),
+							'type'        => 'number',
+							'context'     => [ 'view', 'edit' ],
+							'readonly'    => true,
+						],
+						'name' => [
+							'description' => __( 'Brand name', 'woocommerce' ),
+							'type'        => 'string',
+							'context'     => [ 'view', 'edit' ],
+							'readonly'    => true,
+						],
+						'slug' => [
+							'description' => __( 'Brand slug', 'woocommerce' ),
+							'type'        => 'string',
+							'context'     => [ 'view', 'edit' ],
+							'readonly'    => true,
+						],
+						'link' => [
+							'description' => __( 'Brand link', 'woocommerce' ),
 							'type'        => 'string',
 							'context'     => [ 'view', 'edit' ],
 							'readonly'    => true,
@@ -505,6 +539,7 @@ class ProductSchema extends AbstractSchema {
 			'images'              => $this->get_images( $product ),
 			'categories'          => $this->get_term_list( $product, 'product_cat' ),
 			'tags'                => $this->get_term_list( $product, 'product_tag' ),
+			'brands'              => $this->get_term_list( $product, 'product_brand' ),
 			'attributes'          => $this->get_attributes( $product ),
 			'variations'          => $this->get_variations( $product ),
 			'grouped_products'    => $this->get_grouped_products( $product ),
@@ -618,7 +653,7 @@ class ProductSchema extends AbstractSchema {
 		$attributes                  = array_filter( $product->get_attributes(), [ $this, 'filter_variation_attribute' ] );
 		$default_variation_meta_data = array_reduce(
 			$attributes,
-			function( $defaults, $attribute ) use ( $product ) {
+			function ( $defaults, $attribute ) use ( $product ) {
 				$meta_key              = wc_variation_attribute_name( $attribute->get_name() );
 				$defaults[ $meta_key ] = [
 					'name'  => wc_attribute_label( $attribute->get_name(), $product ),
@@ -667,7 +702,7 @@ class ProductSchema extends AbstractSchema {
 		 */
 		$attributes_by_variation = array_reduce(
 			$variation_meta_data,
-			function( $values, $data ) use ( $default_variation_meta_keys ) {
+			function ( $values, $data ) use ( $default_variation_meta_keys ) {
 				// The query above only includes the keys of $default_variation_meta_data so we know all of the attributes
 				// being processed here apply to this product. However, we need an additional check here because the
 				// cache may have been primed elsewhere and include keys from other products.

commit b4a3b470f4eb336f884e4576bc8bc74d75886ac0

fix: regression in #55305
There is no guarantee that the $request parameter is an array

In fact most of the time it's not, which would throw an exception

plugins/woocommerce/src/StoreApi/Utilities/ProductQuery.php
@@ -123,7 +123,7 @@ class ProductQuery {
 		// Set tax_query for each passed arg.
 		foreach ( $taxonomies as $taxonomy => $key ) {
 			if ( ! empty( $request[ $key ] ) ) {
-				$type        = is_numeric( $request[ $key ][0] ) ? 'term_id' : 'slug';
+				$type        = is_numeric( is_array($request[$key] ) ? current( $request[ $key ] ) ) : $request[ $key ] ) ? 'term_id' : 'slug';
 				$operator    = $request->get_param( $key . '_operator' ) && isset( $operator_mapping[ $request->get_param( $key . '_operator' ) ] ) ? $operator_mapping[ $request->get_param( $key . '_operator' ) ] : 'IN';
 				$tax_query[] = array(
 					'taxonomy' => $taxonomy,